98d0d460f0830a1a253dabbac246cca183d0a74f
Kyle Maxwell <kyle@kylemaxwell.com>
2011-01-22 15:41:28 -0800

    initial port

---
+++ project/build/PQProject.scala
@@ -0,0 +1,14 @@
+import sbt._
+
+class PQProject(info: ProjectInfo) extends DefaultProject(info) {
+  val slf4j_api = "org.slf4j" % "slf4j-api" % "1.5.2"
+  val slf4j_jdk14 = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
+
+  val twitterActors = "com.twitter" % "twitteractors_2.8.0" % "2.0.1"
+  val twitterUtil = "com.twitter" % "util_2.8.0" % "1.2.2"
+  
+  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
+  
+  val scalatools = "Scala Tools" at "http://scala-tools.org/repo-releases/"
+  val twitter = "Scala Tools" at "http://maven.twttr.com/"
+}
---
+++ src/main/scala/com/kylemaxwell/pq/QItem.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2009 Twitter, Inc.
+ * Copyright 2009 Robey Pointer <robeypointer@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kylemaxwell.pq
+
+import java.nio.{ByteBuffer, ByteOrder}
+import com.twitter.util.Time
+
+case class QItem(addTime: Long, expiry: Long, data: Array[Byte], var xid: Int) {
+  def pack(opcode: Byte, withXid: Boolean): ByteBuffer = {
+    val headerSize = if (withXid) 9 else 5
+    val buffer = ByteBuffer.allocate(data.length + 16 + headerSize)
+    buffer.order(ByteOrder.LITTLE_ENDIAN)
+    buffer.put(opcode)
+    if (withXid) {
+      buffer.putInt(xid)
+    }
+    buffer.putInt(data.length + 16)
+    buffer.putLong(addTime)
+    buffer.putLong(expiry)
+    buffer.put(data)
+    buffer.flip()
+    buffer
+  }
+}
+
+object QItem {
+  def unpack(data: Array[Byte]): QItem = {
+    val buffer = ByteBuffer.wrap(data)
+    val bytes = new Array[Byte](data.length - 16)
+    buffer.order(ByteOrder.LITTLE_ENDIAN)
+    val addTime = buffer.getLong
+    val expiry = buffer.getLong
+    buffer.get(bytes)
+    QItem(addTime, expiry, bytes, 0)
+  }
+
+  def unpackOldAdd(data: Array[Byte]): QItem = {
+    val buffer = ByteBuffer.wrap(data)
+    val bytes = new Array[Byte](data.length - 4)
+    buffer.order(ByteOrder.LITTLE_ENDIAN)
+    val expiry = buffer.getInt
+    buffer.get(bytes)
+    QItem(Time.now.inMilliseconds, if (expiry == 0) 0 else expiry * 1000, bytes, 0)
+  }
+}
