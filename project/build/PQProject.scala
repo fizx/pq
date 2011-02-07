fadf1b1e6b9a4cfae9de292be21825bd97a331f5
Kyle Maxwell <kmaxwell@twitter.com>
2011-02-07 13:22:48 -0800

    updated for 2.8.1

--- project/build/PQProject.scala
+++ project/build/PQProject.scala
@@ -1,8 +1,11 @@
 import sbt._
+import com.twitter.sbt._
 
-class PQProject(info: ProjectInfo) extends DefaultProject(info) {
-  val slf4j_api = "org.slf4j" % "slf4j-api" % "1.5.2"
-  val slf4j_jdk14 = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
+class PQProject(info: ProjectInfo) extends StandardProject(info) 
+                                           with SubversionPublisher 
+                                           with AdhocInlines {
+  val slf4j_api = "org.slf4j" % "slf4j-api" % "1.5.2" intransitive()
+  val slf4j_jdk14 = "org.slf4j" % "slf4j-jdk14" % "1.5.2" intransitive()
 
   val twitterActors = "com.twitter" % "twitteractors_2.8.0" % "2.0.1"
   val twitterUtil = "com.twitter" % "util_2.8.0" % "1.2.2"
